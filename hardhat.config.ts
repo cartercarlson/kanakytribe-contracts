e6681d79ffdda9cbd27c4d9eafdc56be20ccb524
Carter Carlson <cartercarlson@protonmail.com>
2022-04-24 11:20:31 -0700

    fix: version, private key

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,26 +6,33 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@typechain/hardhat";
 import "@nomiclabs/hardhat-ethers";
 import "hardhat-gas-reporter";
-import "solidity-coverage";
 import "@nomiclabs/hardhat-etherscan";
 import "hardhat-abi-exporter";
 
 dotenv.config();
+const {
+    PRIVATE_KEY,
+    POLYGONSCAN_API_KEY,
+} = process.env;
 
 const config: HardhatUserConfig = {
     networks: {
         hardhat: {
         },
-        matic: {
+        mumbai: {
             url: "https://rpc-mumbai.maticvigil.com",
-            accounts: [process.env.PRIVATE_KEY]
+            accounts: [`0x${PRIVATE_KEY}`]
+        },
+        matic: {
+            url: "https://rpc-mainnet.maticvigil.com",
+            accounts: [`0x${PRIVATE_KEY}`]
         }
     },
     etherscan: {
-        apiKey: process.env.POLYGONSCAN_API_KEY
+        apiKey: POLYGONSCAN_API_KEY
     },
     solidity: {
-        version: "0.7.0",
+        version: "0.8.12",
         settings: {
             optimizer: {
                 enabled: true,
